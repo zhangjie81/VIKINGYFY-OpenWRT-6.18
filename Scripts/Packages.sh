83a98717532e893e08f3882ca3a8eedc084fa38f
ZJGG <130850482+JGG-SH@users.noreply.github.com>
2026-05-10 02:40:24 +0800

    Remove kucat and kucat-config package updates
    Removed kucat and kucat-config package updates from the script.

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -48,8 +48,6 @@ UPDATE_PACKAGE() {
 
 # UPDATE_PACKAGE "包名" "项目地址" "项目分支" "pkg/name，可选，pkg为从大杂烩中单独提取包名插件；name为重命名为包名"
 UPDATE_PACKAGE "argon" "sbwml/luci-theme-argon" "openwrt-25.12"
-UPDATE_PACKAGE "kucat" "sirpdboy/luci-theme-kucat" "master"
-UPDATE_PACKAGE "kucat-config" "sirpdboy/luci-app-kucat-config" "master"
 UPDATE_PACKAGE "4lpha" "derisamedia/luci-theme-alpha-reborn" "main"
 
 UPDATE_PACKAGE "passwall" "Openwrt-Passwall/openwrt-passwall" "main" "pkg"
