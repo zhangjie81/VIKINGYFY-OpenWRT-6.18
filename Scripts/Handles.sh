8d6550f87ae57f7bfec9c8d647ed9ecbbcb75e8a
ZJGG <130850482+JGG-SH@users.noreply.github.com>
2026-05-10 11:31:50 +0800

    Update argon theme primary color in Handles.sh

--- Scripts/Handles.sh
+++ Scripts/Handles.sh
@@ -8,7 +8,7 @@ PKG_PATH="$GITHUB_WORKSPACE/wrt/package/"
 if [ -d *"luci-theme-argon"* ]; then
 	echo " " && cd ./luci-theme-argon/
 
-	sed -i "s/primary '.*'/primary '#31a1a1'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
+	sed -i "s/primary '.*'/primary '#555fc8'/; s/'0.2'/'0.5'/; s/'none'/'bing'/; s/'600'/'normal'/" ./luci-app-argon-config/root/etc/config/argon
 
 	cd $PKG_PATH && echo "theme-argon has been fixed!"
 fi
