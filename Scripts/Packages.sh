d05d69de7b9e1897d1b768477feb1634f7d14693
ZJGG <130850482+JGG-SH@users.noreply.github.com>
2026-05-10 16:34:59 +0800

    Update Packages.sh

--- Scripts/Packages.sh
+++ Scripts/Packages.sh
@@ -53,7 +53,7 @@ UPDATE_PACKAGE "filemanager" "sbwml/luci-app-filemanager" "main"
 UPDATE_PACKAGE "luci-app-tailscale" "asvow/luci-app-tailscale" "main"
 UPDATE_PACKAGE "easytier" "EasyTier/luci-app-easytier" "main"
 UPDATE_PACKAGE "openlist2" "sbwml/luci-app-openlist2" "main"
-UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "master" "pkg"
+UPDATE_PACKAGE "lucky" "gdy666/luci-app-lucky" "main" "pkg"
 
 #更新软件包版本
 UPDATEVERSION() {
